39ad779c9bda7918a303db63bb8c5b2064672a2a
aklesh-dev <arzunayavadu998@gmail.com>
2025-03-09 21:24:00 +0545

    feat(auth): login functionality - Implement auth and user role middleware.

--- 17.Authentication-and-Authorization/controllers/auth.controller.js
+++ 17.Authentication-and-Authorization/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const User = require('../models/user.model');
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
   try {
@@ -13,10 +14,10 @@ const registerUser = async (req, res) => {
       if (checkExistingUser.username) {
         message += 'username';
       }
-      else if(checkExistingUser.email){
+      else if (checkExistingUser.email) {
         message += 'email'
       }
-      else if(checkExistingUser.email && checkExistingUser.username) {
+      else if (checkExistingUser.email && checkExistingUser.username) {
         message += 'username and email';
       }
       return res.status(400).json({
@@ -56,6 +57,29 @@ const registerUser = async (req, res) => {
 
 const loginUser = async (req, res) => {
   try {
+    // Extract user credential from request body
+    const { username, password } = req.body;
+
+    // Check user exists in the database
+    const validUser = await User.findOne({ username });
+    if (!validUser) return res.status(404).json({ success: false, message: 'User not found' });
+
+    // Check and compare the password
+    const isPasswordMatch = bcrypt.compareSync(password, validUser.password);
+    if (!isPasswordMatch) return res.status(403).json({ success: false, message: "Invalid credentials" });
+
+    // Create user token
+    const accessToken = jwt.sign({
+      userId: validUser._id,
+      username: validUser.username,
+      role: validUser.role,
+    }, process.env.JWT_SECRET, { expiresIn: '15m' });
+
+    return res.status(200).json({
+      success: true,
+      message: "User logged successfully",
+      accessToken,
+    });
 
   } catch (error) {
     console.error('Error logging user:', error);
--- 17.Authentication-and-Authorization/server.js
+++ 17.Authentication-and-Authorization/server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const connectToDB = require('./database/db');
 const authRoutes = require('./routes/auth.route');
+const homeRoutes = require('./routes/home.route');
+const adminRoutes = require('./routes/admin.route');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,7 +12,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes);
+app.use('/api/home', homeRoutes);
+app.use('/api/admin', adminRoutes);
 
 // Database connection
 connectToDB(); 
